c5b0cf3dd2f0a1c1e424bd5b163c9b48cb4b81da
me <kopecvo3@fit.cvut.cz>
2022-11-22 19:07:03 +0100

    Upload to backend

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,30 +1,70 @@
 <template>
-  <v-btn
-    size="large"
-    color="success"
-    variant="tonal"
-    :disabled="sequencerStore.getMelodyNotes < store.minMelodyLengthSearch"
-    @click="search"
+  <div
+    class="search"
   >
-    Search
-  </v-btn>
-  <h4>Or...</h4>
-  <v-btn
-    color="success"
-    variant="tonal"
-    :disabled="true"
-  >
-    Search MIDI file
-  </v-btn>
+    <div
+      style="width: 250px"
+    >
+      <v-form
+        ref="uploadMidiForm"
+        v-model="uploadMidiValid"
+      >
+        <v-file-input
+          @change="setFiles"
+          :rules="uploadMidiRules"
+          accept=".mid"
+          label="Choose a MIDI file"
+          density="compact"
+          required
+        />
+      </v-form>
+
+    </div>
+    <v-btn
+      class="item"
+      size="large"
+      color="success"
+      variant="tonal"
+      :disabled="sequencerStore.getMelodyNotes < store.minMelodyLengthSearch"
+      @click="search"
+    >
+      Search
+    </v-btn>
+
+    <v-btn
+      class="item"
+      size="large"
+      color="success"
+      variant="tonal"
+      :disabled="!uploadMidiValid"
+      @click="upload"
+    >
+      Search MIDI
+    </v-btn>
+  </div>
 </template>
 
 <script setup>
     import {useSequencerStore} from "@/store/sequencerStore";
     import {useStore} from "@/store/store";
     import axios from "axios"
+    import {reactive, ref, watch} from "vue";
 
     const sequencerStore = useSequencerStore()
     const store = useStore()
+
+    let midiFiles = reactive([])
+    let uploadMidiValid = ref(false)
+    let uploadMidiRules = [
+        value => {
+            return !value || !value.length || value[0].size < 1000000 || 'MIDI size should be less than 1 MB!'
+        },
+    ]
+
+    function setFiles(e) {
+        midiFiles = e.target.files
+        console.log("setting files")
+    }
 
     function search() {
         axios.post('http://127.0.0.1:8000/api/', {
@@ -37,8 +77,24 @@
                 console.log(error);
             });
     }
+
+    function upload() {
+        let formData = new FormData();
+        formData.append("file", midiFiles[0]);
+        axios.post('http://127.0.0.1:8000/api/upload/', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(response => console.log(response)).catch(error => console.log(error))
+    }
+
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+.search
+  display: flex
+  flex-direction: row
 
+.item
+  margin-left: 5px
 </style>
